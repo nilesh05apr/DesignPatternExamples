24d4fbd8b85ac54ff7b8683e06e1e601adb4e522
nilesh05apr <nk8588338@gmail.com>
2022-09-07 05:12:50 +0530

    add: node example:

--- SingletonDesignPattern/Node/controllers/logdata.js
+++ SingletonDesignPattern/Node/controllers/logdata.js
@@ -3,10 +3,6 @@ const logger = require('../logger/logger');
 let logdata = {
     logdata: (req,res) => {
         if(req.method == 'GET') {
-            let date = new Date();
-            logger.logCount()
-            logger.log(`${date} : logdata service called.`)
-            logger.logCount()
             res.status(200).json({"logdata":logger.logs})
         } else {
             res.status(400)
